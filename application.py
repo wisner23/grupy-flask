b355da5dc2936a5ee4af715d77986cb3b894c35f
wisner.oliveira <wisner.oliveira@gmail.com>
2017-11-16 20:22:20 -0200

    Initial Commit

---
+++ application.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+
+@application.route("/", methods=["GET"])
+def get_hello_world():
+    return "olá mundo"
+
+
+if __name__ == "__main__":
+    application.run(host="0.0.0.0", port=8080, debug=True)
+
+
+
+ 
